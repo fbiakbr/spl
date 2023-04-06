4d238a89d2631edf2d5d36450fc2e73673137cf9
fbiakbr <febiakbar19@gmail.com>
2023-04-06 10:28:49 +0700

    update db

--- db/spl.sql
+++ db/spl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2023 at 05:46 PM
+-- Generation Time: Apr 06, 2023 at 05:28 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -101,7 +101,29 @@ INSERT INTO `auth_logins` (`id`, `ip_address`, `email`, `user_id`, `date`, `succ
 (10, '::1', 'a', NULL, '2023-04-05 15:30:05', 0),
 (11, '::1', 'admin', NULL, '2023-04-05 15:30:13', 0),
 (12, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 15:30:22', 1),
-(13, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 15:31:01', 1);
+(13, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 15:31:01', 1),
+(14, '::1', 'admin', NULL, '2023-04-05 16:24:30', 0),
+(15, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:24:36', 1),
+(16, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:29:46', 1),
+(17, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:31:36', 1),
+(18, '::1', 'a', NULL, '2023-04-05 16:33:33', 0),
+(19, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:33:38', 1),
+(20, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:34:43', 1),
+(21, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:35:53', 1),
+(22, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:37:15', 1),
+(23, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:37:46', 1),
+(24, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:38:20', 1),
+(25, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:38:40', 1),
+(26, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:46:02', 1),
+(27, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:47:09', 1),
+(28, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:47:30', 1),
+(29, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:47:46', 1),
+(30, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:50:43', 1),
+(31, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:51:00', 1),
+(32, '::1', 'fbiakbr@gmail.com', 2, '2023-04-05 16:51:22', 1),
+(33, '127.0.0.1', 'admin', NULL, '2023-04-06 02:15:28', 0),
+(34, '127.0.0.1', 'fbiakbr@gmail.com', 2, '2023-04-06 02:15:34', 1),
+(35, '127.0.0.1', 'fbiakbr@gmail.com', 2, '2023-04-06 02:46:18', 1);
 
 -- --------------------------------------------------------
 
@@ -196,6 +218,14 @@ CREATE TABLE `pemakaian` (
   `materi_praktek` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `pemakaian`
+--
+
+INSERT INTO `pemakaian` (`id_pemakaian`, `nis`, `nama_siswa`, `kelas`, `tanggal`, `jam`, `lab`, `no_pc`, `materi_praktek`) VALUES
+(40, 6120, 'M KHOIRUL ANAM', 'XI TJKT 2', '2023-04-06', '10:01:26', 'Lab TKJ L6', 1, 'Konfigurasi router'),
+(41, 6282, 'AHMAD AZRIL AFIF', 'XI TJKT 1', '2023-04-06', '10:05:51', 'Lab TKJ L6', 1, 'Konfigurasi web server');
+
 -- --------------------------------------------------------
 
 --
@@ -213,14 +243,60 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`nis`, `nama_siswa`, `kelas`) VALUES
-(1657, 'Kezia Mardhiyah', 'XI TKJ 2'),
-(1675, 'Paulin Mayasari', 'X TKJ 1'),
-(2246, 'Lala Nuraini', 'X TKJ 1'),
-(2350, 'Qori Pudjiastuti', 'XI TKJ 1'),
-(4676, 'Febiadi Wisnu Akbar', 'XII TKJ 1'),
-(5424, 'Cayadi Natsir', 'XI TKJ 1'),
-(7872, 'Laksana Simanjuntak', 'XII TKJ 2'),
-(8735, 'Mala Puspita', 'XI TKJ 2');
+(6120, 'M KHOIRUL ANAM', 'XI TJKT 2'),
+(6282, 'AHMAD AZRIL AFIF', 'XI TJKT 1'),
+(6283, 'ANGGELI WULAN SAFITRI', 'XI TJKT 1'),
+(6284, 'ASSEGAF', 'XI TJKT 1'),
+(6285, 'DEVI AZIFATUL AZMI', 'XI TJKT 1'),
+(6286, 'DINA NOVIANA', 'XI TJKT 1'),
+(6288, 'EMA FATIMATUZZAHRO', 'XI TJKT 1'),
+(6289, 'FATKHUL MANAN', 'XI TJKT 1'),
+(6290, 'M FADHIL FERDIANSYAH', 'XI TJKT 1'),
+(6291, 'M. WILDAN ILMI', 'XI TJKT 1'),
+(6292, 'M.ANDIKA AL FAIZIN', 'XI TJKT 1'),
+(6293, 'M.FIKRI ARIS SETIAWAN', 'XI TJKT 1'),
+(6294, 'M.RIZQI ALFATDILLAH', 'XI TJKT 1'),
+(6295, 'MAULANA FAJAR RAHMAN SIDIQ', 'XI TJKT 1'),
+(6296, 'MOHAMMAD ABDUL HALIM', 'XI TJKT 1'),
+(6297, 'MUHAMAD IPNU FAIQ', 'XI TJKT 1'),
+(6298, 'MUHAMMAD IQBAL SURYA ADI', 'XI TJKT 1'),
+(6299, 'NUR HIDAYAH', 'XI TJKT 1'),
+(6300, 'NURUL ROJAFINA', 'XI TJKT 1'),
+(6301, 'PANJI SALMAN AL FARISI', 'XI TJKT 1'),
+(6302, 'REVA OKTAVIA', 'XI TJKT 1'),
+(6303, 'SASKIA RAYA WULAN', 'XI TJKT 1'),
+(6304, 'SITI DALILAH MUSTAQIMAH', 'XI TJKT 1'),
+(6305, 'SULISTIO NUGROHO', 'XI TJKT 1'),
+(6306, 'TRI ISNAINI', 'XI TJKT 1'),
+(6307, 'ACHMAD BACHRUL ULUM', 'XI TJKT 2'),
+(6309, 'ALDO ALDINO FEBRIAN ADHA', 'XI TJKT 2'),
+(6310, 'ARYA AGUNG PRATAMA', 'XI TJKT 2'),
+(6311, 'AYU KURNIANINGSIH', 'XI TJKT 2'),
+(6312, 'DINA FAZA AMALIA', 'XI TJKT 2'),
+(6313, 'ELA SAFANA', 'XI TJKT 2'),
+(6314, 'FAREL ALISSANAYA', 'XI TJKT 2'),
+(6315, 'FRIZY ANANDA', 'XI TJKT 2'),
+(6316, 'ISTIANA RISQI', 'XI TJKT 2'),
+(6317, 'M. AUVAL HANA', 'XI TJKT 2'),
+(6318, 'M.ALEIF FATIH', 'XI TJKT 2'),
+(6319, 'M.ASYRIL MAULA', 'XI TJKT 2'),
+(6320, 'M.IBROHIM', 'XI TJKT 2'),
+(6321, 'M.TAUFIK HIDAYAT', 'XI TJKT 2'),
+(6322, 'MOHAMAD YUDIS ALFAREZI', 'XI TJKT 2'),
+(6323, 'MOHAMMAD NURFAHROZI', 'XI TJKT 2'),
+(6324, 'MUHAMMAD ARJUNNAJAH', 'XI TJKT 2'),
+(6325, 'MUHAMMAD ZAHRIE TIYYAS HABIBIE', 'XI TJKT 2'),
+(6326, 'NURJANAH', 'XI TJKT 2'),
+(6327, 'PUTRI ANDINI', 'XI TJKT 2'),
+(6328, 'RINA WATI', 'XI TJKT 2'),
+(6329, 'SAJI TRI WIJOYO', 'XI TJKT 2'),
+(6330, 'SEPTIA IKFI TANIA', 'XI TJKT 2'),
+(6331, 'TIWI ARINI', 'XI TJKT 2'),
+(6332, 'YUSTIFAN ARIF HIDAYATULLAH', 'XI TJKT 2'),
+(6335, 'NUNUNG NOVIANI', 'XI TJKT 1'),
+(6337, 'MUHAMMAD GHOZALI', 'XI TJKT 2'),
+(6340, 'NASRUL ANNAM', 'XI TJKT 1'),
+(6342, 'VITA NABILA', 'XI TJKT 2');
 
 -- --------------------------------------------------------
 
@@ -358,13 +434,13 @@ ALTER TABLE `auth_activation_attempts`
 -- AUTO_INCREMENT for table `auth_groups`
 --
 ALTER TABLE `auth_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `auth_logins`
 --
 ALTER TABLE `auth_logins`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `auth_permissions`
@@ -394,7 +470,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `pemakaian`
 --
 ALTER TABLE `pemakaian`
-  MODIFY `id_pemakaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_pemakaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `users`
